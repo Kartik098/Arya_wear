d6c991ceb24ed249dd031d0a3286a81297eb66fd
Pc <vraj12082001@gmail.com>
2023-04-20 10:11:37 +0530

    register_screen

---
+++ lib/register.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class register_Screen extends StatefulWidget {
+  const register_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<register_Screen> createState() => _register_ScreenState();
+}
+
+class _register_ScreenState extends State<register_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
